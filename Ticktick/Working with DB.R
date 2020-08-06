81114b8f60b0e2154a4a02bc4d4da67f346385e4
Michael Lagunov <lagunovmike@hotmail.com>
2020-08-07 00:10:15 +0700

    Made new csv entries append to the DB

--- Ticktick/Working with DB.R
+++ Ticktick/Working with DB.R
@@ -15,6 +15,7 @@ rs <- dbSendQuery(db,
         "INSERT INTO Tasks ('Title', 'Priority', 'Created.time', 'Completed.time')
         VALUES (?,?,?,?);
         ")
+rs
 dbBind(rs, list(lastmonth$title, lastmonth$priority, lastmonth$created.time, 
        lastmonth$completed.time))
 dbClearResult(rs)
@@ -27,3 +28,20 @@ dbDisconnect(db)
 names(a_data) <- names(lastmonth)
 a_data <- slice(a_data, -nrow(a_data))
 setdiff(lastmonth$created.time, a_data$Created.time)
+
+
+
+
+
+
+
+
+
+
+
+###
+db <- dbConnect(RSQLite::SQLite(), dbname = "Ticktick/ticktickDB.db", encoding = "UTF-8")
+dbExecute(db, "CREATE TABLE tt_demo AS SELECT * FROM ticktick")
+dbExecute(db, "DROP TABLE tt_demo")
+dbDisconnect(db)
+
