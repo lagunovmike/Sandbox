e9070f4af8ee32f9ba7a478dafcf7b46a32e3584
Michael Lagunov <lagunovmike@hotmail.com>
2020-08-08 02:37:50 +0700

    moved queries into functions (added functons.R)

--- Ticktick/Ticktick.R
+++ Ticktick/Ticktick.R
@@ -1,62 +1,17 @@
 library(lubridate)
 library(tidyverse)
 library(DBI)
-library(dbplyr)
+source("Ticktick/functions.R")
 
 #### Reading data
-fileDest <- str_sort(list.files("Ticktick/backups", full.names = TRUE), 
-                     decreasing = TRUE)[1]
-ticktick <- read_csv(fileDest, skip = 3, locale = locale(encoding = "UTF-8"))
-id <- c(seq(1:nrow(ticktick)))
-ticktick <- cbind(id,ticktick)
-names(ticktick)[1] <- "ID"
-names(ticktick) <- gsub(" ", "", names(ticktick))
+latest_backup <- read_latest()
+# Update database
+updateDB()
 
-### Connect to the db
-db <- dbConnect(RSQLite::SQLite(), dbname = "Ticktick/ticktickDB.db")
+# Get data from a period ("all" or "last month")
+tt_db <- getdata()
 
-#################################
-### Check whether csv equal to the db, otherwise append new entries
-db_nrow <- dbGetQuery(db, "SELECT COUNT(ID) as count FROM ttdemo")
-if(nrow(ticktick) != db_nrow$count){
-    dbID <- dbGetQuery(db,"SELECT ID FROM ttdemo");
-    # Look for changes
-    new_short <- setdiff(ticktick$ID, dbID$ID)
-    new_full <- filter(ticktick, ID %in% new_short)
-    ## Append into the db
-    dbAppendTable(db, name = "ttdemo", value = new_full)
-} else{
-    cat("Database is up to date")
-}
-
-# Get last month tasks
-lastm_q <- dbSendQuery(db, "
-          SELECT
-          Title,
-          Priority,
-          DATETIME (CreatedTime, 'unixepoch') as cr,
-          DATETIME (DueDate, 'unixepoch') as dd,
-          DATETIME (CompletedTime, 'unixepoch') as com
-          FROM ttdemo
-          WHERE cr >= strftime('%Y-%m-%d', ?)
-          OR com >= strftime('%Y-%m-%d', ?)
-          OR dd >= strftime('%Y-%m-%d', ?)",
-                       params = rep(as.character(today()-months(1)),3))
-lastm <- dbFetch(lastm_q)
-RSQLite::dbClearResult(lastm_q)
-dbDisconnect(db)
-
-# Handling DueDate issue
-# Sometimes DueDate is smaller than the creation date
-lastm$dd <- as_datetime(lastm$dd)
-for(i in 1:nrow(lastm)){
-    if(!is.na(lastm$dd[i])){
-        if(date(ymd_hms(lastm$dd[i])) < date(ymd_hms(lastm$cr[i]))){
-            lastm$dd[i] <- ymd_hms(lastm$dd[i]) + days(1)
-        }
-    }
-}
-lastmonth <- tibble(lastm) %>%
+ticktick <- tibble(tt_db) %>%
     rename(CreatedTime = cr, DueDate = dd, CompletedTime = com) %>%
     mutate(CreatedTime = ymd_hms(CreatedTime), 
            DueDate = ymd_hms(DueDate), 
@@ -67,11 +22,20 @@ lastmonth <- tibble(lastm) %>%
     mutate(IsCompleted = ifelse(is.na(CompletedTime), 
                                  "not completed", "nompleted")) %>%
     mutate(Priority = factor(Priority, levels = c("0", "1", "3", "5"),
-                            labels = c("None", "Low", "Medium", "High")))
+                            labels = c("None", "Low", "Medium", "High"))) %>%
+    mutate(ComplDelay = date(CompletedTime) - date(DueDate))
     
-lastmonth$Priority <- factor(lastmonth$Priority, levels = c(0,1,3,5),
-                             labels = c(0 = "None", 1 = "Low", 3 = "Medium", 5 = "High"))
-lastmonth
+tasks_by_y <- ticktick %>%
+    select(Title, Priority, DueDate, CreatedTime, ComplDelay) %>%
+    group_by(year = year(CreatedTime), month = month(CreatedTime)) %>%
+    summarise(Tasks = n(), .groups = "drop") %>%
+    unite(YearMon, year:month, remove = TRUE, sep = "-") %>%
+    mutate(YearMon = as.yearmon(YearMon))
+tasks_by_y
+plot(tasks_by_y, type = "l", col = "blue")
+points(tasks_by_y, col = "blue", pch = 19)
+text(x = tasks_by_y$YearMon,y = tasks_by_y$Tasks, labels = tasks_by_y$Tasks, pos = 3,
+     col = "magenta", cex = 0.8)
 # Completion Rate
 tasks <- nrow(lastmonth)
 completed <- sum(!is.na(lastmonth$completed.time))
---
+++ Ticktick/functions.R
@@ -0,0 +1,77 @@
+read_latest <- function(path = "Ticktick/backups"){
+    fileDest <- str_sort(list.files(path, full.names = TRUE), 
+                         decreasing = TRUE)[1]
+    ticktick <- read_csv(fileDest, skip = 3, locale = locale(encoding = "UTF-8"))
+    id <- c(seq(1:nrow(ticktick)))
+    ticktick <- cbind(id,ticktick)
+    names(ticktick)[1] <- "ID"
+    names(ticktick) <- gsub(" ", "", names(ticktick))
+    cat("Done")
+    return(ticktick)
+}
+
+
+updateDB <- function(backupFile = latest_backup){
+    db <- dbConnect(RSQLite::SQLite(), dbname = "Ticktick/ticktickDB.db")
+    db_nrow <- dbGetQuery(db, "SELECT COUNT(ID) as count FROM ttdemo")
+    if(nrow(backupFile) != db_nrow$count){
+        dbID <- dbGetQuery(db,"SELECT ID FROM ttdemo");
+        # Look for changes
+        new_short <- setdiff(backupFile$ID, dbID$ID)
+        new_full <- filter(backupFile, ID %in% new_short)
+        ## Append into the db
+        dbAppendTable(db, name = "ttdemo", value = new_full)
+        dbDisconnect(db)
+        cat(paste("Updated:", length(new_short), "new enrties"))
+    } else{
+        cat("Database is up to date")
+    }
+}
+
+
+getdata <- function(period = "all"){
+    db <- dbConnect(RSQLite::SQLite(), dbname = "Ticktick/ticktickDB.db")
+    if(period == "all"){
+        getdata_q <- dbSendQuery(db,"
+          SELECT
+          Title,
+          Priority,
+          DATETIME (CreatedTime, 'unixepoch') as cr,
+          DATETIME (DueDate, 'unixepoch') as dd,
+          DATETIME (CompletedTime, 'unixepoch') as com
+          FROM ttdemo;")
+    } else if (period == "last month"){
+        getdata_q <- dbSendQuery(db,"
+          SELECT
+          Title,
+          Priority,
+          DATETIME (CreatedTime, 'unixepoch') as cr,
+          DATETIME (DueDate, 'unixepoch') as dd,
+          DATETIME (CompletedTime, 'unixepoch') as com
+          FROM ttdemo
+          WHERE cr >= strftime('%Y-%m-%d', ?)
+          OR com >= strftime('%Y-%m-%d', ?)
+          OR dd >= strftime('%Y-%m-%d', ?);
+                             ",
+                                 params = rep(as.character(today()-months(1)),3))
+    } else {
+        stop("Wrong period!")
+    }
+    tt_data <- dbFetch(getdata_q)
+    RSQLite::dbClearResult(getdata_q)
+    dbDisconnect(db)
+    
+    # Fixing DueDate issue
+    tt_data$dd <- as_datetime(tt_data$dd)
+    tt_data$cr <- as_datetime(tt_data$cr)
+    tt_data$com <- as_datetime(tt_data$com)
+    for(i in 1:nrow(tt_data)){
+        if(!is.na(tt_data$dd[i])){
+            if(date(ymd_hms(tt_data$dd[i])) < date(ymd_hms(tt_data$cr[i]))){
+                tt_data$dd[i] <- ymd_hms(tt_data$dd[i]) + days(1)
+            }
+        }
+    }
+    cat("Done")
+    return(tt_data)
+}
