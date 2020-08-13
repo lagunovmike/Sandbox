450d321625854d9f9295206d045949717b85cf46
Michael Lagunov <lagunovmike@hotmail.com>
2020-08-13 14:38:43 +0700

    added analyzing by day, by wday, by hour

--- TJournal/tj_analyze.R
+++ TJournal/tj_analyze.R
@@ -10,28 +10,27 @@ Sys.setlocale("LC_CTYPE", "russian") #solve encoding issues
 db <- dbConnect(RSQLite::SQLite(), "news.db")
 db_data <- dbGetQuery(db, "SELECT * FROM tjournal")
 dbDisconnect(db)
-
-pub_date <- date(as_datetime(db_data$pub_date))
+db_data$pub_date <- as_datetime(db_data$pub_date, tz = "Europe/Moscow")
+pub_date <- date(db_data$pub_date)
 
 b_date <- tapply(pub_date, date(pub_date), length)
 
-comb <- data.frame(day = unique(pub_date), count = b_date, row.names = seq_along(b_date))
-#comb <- comb[-which.max(comb$day),]
-comb <- arrange(comb, day) %>%
-    mutate(rollmean = rollmean(x = count, k = 7, fill = "extend"))
+by_day <- data.frame(day = unique(pub_date), count = b_date, row.names = seq_along(b_date))
+by_day <- arrange(by_day, day) %>%
+    mutate(rollmean = round(rollmean(x = count, k = 7, fill = "extend"),2))
 
-with(comb, plot(day, count))
-with(comb, lines(day, rollmean, col = "red"))
-abline(h = mean(comb$count), col = "blue")
+with(by_day, plot(day, count))
+with(by_day, lines(day, rollmean, col = "red"))
+abline(h = mean(by_day$count), col = "blue")
 
 # check a peak in the middle of 2015
-alot <- comb[which.max(comb$count),]$day
-alot_row <- filter(db_data, date(as_datetime(pub_date)) == alot)
+alot <- by_day[which.max(by_day$count),]$day
+alot_row <- filter(db_data, date(pub_date) == alot)
 # answer: some spam, deleted articles
 
-summary(comb$count)
-sd(comb$count)
-hist(comb$count)
+summary(by_day$count)
+sd(by_day$count)
+hist(by_day$count)
 # check whether there are days with zero entries
 di <- diff(unique(pub_date[order(pub_date)]))
 unique(di)[-(di==1)]
@@ -42,7 +41,21 @@ deleted <- filter(db_data, title == "Статья удалена")
 nrow(deleted)/nrow(db_data)
 
 not_del <- filter(db_data, title != "Статья удалена") %>%
-    group_by(date(as_datetime(pub_date))) %>%
-    summarize(count_d = n())
+    group_by(date(pub_date)) %>%
+    summarize(count_d = n(), .groups = "drop")
 
-hist(not_del$count_d)    
+hist(not_del$count_d)
+
+by_wday <- db_data %>%
+    group_by(wday = wday(pub_date, label = TRUE, week_start = 1, abbr = FALSE)) %>%
+    summarize(count_wday = n(), .groups = "drop") %>%
+    mutate(wday = as.factor(wday))
+barplot(by_wday$count_wday~by_wday$wday)
+
+by_hms <- db_data %>%
+    group_by(pub_time = hour(pub_date)) %>%
+    summarize(count = n(), .groups = "drop")
+
+
+plot(by_hms$pub_time, by_hms$count, pch = 19)
+lines(by_hms$pub_time, by_hms$count)
