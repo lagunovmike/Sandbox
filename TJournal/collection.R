906dedad75918d629376a7fff1c149ece4089175
Michael Lagunov <lagunovmike@hotmail.com>
2020-08-13 13:25:44 +0700

    fixed breaking due to the 429 error

--- TJournal/collection.R
+++ TJournal/collection.R
@@ -1,13 +1,25 @@
-library(httr)
-library(jsonlite)
-library(dplyr)
-library(lubridate)
-library(DBI)
-Sys.setlocale("LC_CTYPE", "russian")
+# Libraries
+library(httr) #connecting to api
+library(jsonlite) #parsing json
+library(lubridate) #date converting
+library(DBI) #connecting to the db
+Sys.setlocale("LC_CTYPE", "russian") #solve encoding issues
 
+### FUNCTIONS
+
+# Api connecting
 tj_api <- function(path){
     url <- modify_url("https://api.tjournal.ru/", path = path)
     resp <- GET(url)
+    
+    # look for the 429 error
+    if(grepl("429 Too Many", resp) == TRUE){
+        cat("\n","429 error, sleeping", "\n")
+        Sys.sleep(120)
+        cat("\n")
+        #try again
+        resp <- GET(url)
+    }
     parsed <- jsonlite::fromJSON(content(resp, "text"), simplifyVector = TRUE,
                                  flatten = TRUE)
     
@@ -25,64 +37,94 @@ tj_api <- function(path){
 get_collection <- function(n = 10, offset = 0){
     
     entries <- list()
+    # set counters and a progress bar
     count_to_db <- 0
-    count_sleep <- 1
-    pb <- txtProgressBar(min = 0, max = 500, initial = 0)
+    count_sleep <- 0
+    count_toomany <- 0
+    pb <- txtProgressBar(min = 0, max = 200, initial = 0)
+    
     for (i in 1:n){
         setTxtProgressBar(pb,count_to_db)
-        if(count_sleep == 200){
+        
+        # sleep for a while every n entries
+        if(count_sleep == 500){
             cat("\n", "resting")
-            Sys.sleep(80)
+            Sys.sleep(20)
             cat("\n")
             count_sleep <- 1
         }
         
         link <- paste0("v1.8/entry/", i+offset)
         get_entry <- tj_api(link)
+        
+        # skip if 404
         if(get_entry$response$status_code == 404){
             count_sleep <- count_sleep + 1
+            Sys.sleep(0.5)
             next  
         } 
+        
+        # data fetching
         id <- get_entry$content$result$id
         title <- get_entry$content$result$title
         brief <- get_entry$content$result$summarize
         link <- get_entry$content$result$url
-        pub_date <- as_datetime(get_entry$content$result$date)
+        pub_date <- get_entry$content$result$date
         hits <- get_entry$content$result$hitsCount
         comment_count <- get_entry$content$result$commentsCount
         favorite <- get_entry$content$result$favoritesCount
         isEditoral <- get_entry$content$result$isEditorial
         isRepost <- get_entry$content$result$isRepost
+        
+        # appending to a list
         entries[[i]] <- c(id = id, title = title, brief = brief, link = link,
                           pub_date = pub_date, hits = hits, 
                           comment_count = comment_count, favorite = favorite, 
                           isEditoral = isEditoral, isRepost = isRepost
                           )
-        
+        # send to the db every n entries
         if(count_to_db == 30){
             count_to_db <- 0
             collection <- do.call(dplyr::bind_rows, entries)
             entries <- list()
-            db <- dbConnect(RSQLite::SQLite(), dbname = "TJournal/news.db")
+            db <- dbConnect(RSQLite::SQLite(), dbname = "news.db")
             dbAppendTable(db, name = "tjournal", value = collection)
             dbDisconnect(db)
-            #flush.console()
-            cat(" Done: ",i)
+            Sys.sleep(2)
+            cat("   Done: ",i)
         }
+        
+        # raise counters
         count_to_db <- count_to_db + 1
         count_sleep <- count_sleep + 1
+        Sys.sleep(0.1)
         
     }
-    collection <- do.call(dplyr::bind_rows, entries)
-    return(collection)
+    db <- dbConnect(RSQLite::SQLite(), dbname = "news.db")
+    parsed_id <- as.vector(dbGetQuery(db, 
+                                      "SELECT MAX(id) as max FROM tjournal")$max)
+    con("Completed:", parsed_id - max_id_db)
+    dbDisconnect(db)
 }
+### END FUNCTION
+
+
 latest_news <- tj_api("v1.8/news/default/recent")
 latest_id <- latest_news$content$result$id[1]
 
-db <- dbConnect(RSQLite::SQLite(), dbname = "TJournal/news.db")
+# Get info before start
+db <- dbConnect(RSQLite::SQLite(), dbname = "news.db")
 max_id_db <- as.vector(dbGetQuery(db, "SELECT MAX(id) as max FROM tjournal")$max)
+last_pub <- as.vector(dbGetQuery(db, 
+                                 "SELECT 
+                                 MAX(DATETIME (pub_date, 'unixepoch')) as pb 
+                                 FROM tjournal")$pb)
 dbDisconnect(db)
 
-cat("Total: ", max_id_db)
-cat("Started id: ", latest_id, " ; Done: ", round(max_id_db/latest_id,2))
+# Place info
+cat("Total:", max_id_db)
+cat("The most recent publication is from:", 
+    as.character(date(as_datetime(last_pub))))
+cat("Started id:", latest_id, "- Finished prop:", round(max_id_db/latest_id,2))
+
 my_collection <- get_collection(sum(latest_id,-max_id_db), offset = max_id_db)
